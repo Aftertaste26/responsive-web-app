4cac1f03db76ca3e95cb07292808a3c7353b2bb6
Aftertaste26 <20hardrock12@gmail.com>
2020-10-27 17:26:25 +0800

    refactor: quiz script

--- script.js
+++ script.js
@@ -1,24 +1,14 @@
-// const R = require("ramda");
-
 window.onload = function onload() {
   const hamburgerIcon = document.querySelector(".hamburger-menu");
   const closeIcon = document.querySelector(".closebtn");
   const startButton = document.querySelector("#quiz-start-btn");
-  const quizStartContainer = document.querySelector("#quiz-start-container");
-  const questionsContainer = document.querySelector(".questions");
-  const nextBtn = document.querySelector("#nextBtn");
-  const cardContainer = document.querySelector(".card-container");
-  const imgContainer = document.createElement("div");
-  const gifImage = document.createElement("img");
-  imgContainer.className = "img-container";
-  let choice;
-  let numberOfQuestions = 5;
-  let count = 0;
 
-  // let answers = [];
+  const quizStat = {
+    chosen: {},
+  };
 
-  const houses = [
-    {
+  const houses = {
+    gryffindor: {
       name: "Gryffindor",
       traits: [
         "Stand up to them",
@@ -29,10 +19,9 @@ window.onload = function onload() {
         "An eye at the keyhole",
         "Draw your wand and try to discover the source of the noise",
       ],
-      tally: 0,
       logo: "assets/",
     },
-    {
+    ravenclaw: {
       name: "Ravenclaw",
       traits: [
         "Let them be, I have better things to do than waste my time on those people",
@@ -43,10 +32,9 @@ window.onload = function onload() {
         "Standing on top of something very high",
         "Withdraw into the shadows to await developments",
       ],
-      tally: 0,
       logo: "assets/",
     },
-    {
+    hufflepuff: {
       name: "Hufflepuff",
       traits: [
         "Runaway and cry, but never tell anyone about them",
@@ -57,10 +45,9 @@ window.onload = function onload() {
         "Your friends nor your family have any idea who you are",
         "Proceed with caution",
       ],
-      tally: 0,
       logo: "assets/",
     },
-    {
+    slytherin: {
       name: "Slytherin",
       traits: [
         "Leave them to what they're doing, I'll tell my father, then plan revenge",
@@ -71,199 +58,254 @@ window.onload = function onload() {
         "Being forced to speak in such a silly voice",
         "Draw your wand and stand your ground",
       ],
-      tally: 0,
       logo: "assets/",
     },
-  ];
+  };
 
   const quizItems = [
     {
       question: "How would you react if someone picks on you and your friend?",
-      // choices: [
-      //   "Stand up to them", //G
-      //   "Leave them to what they're doing, I'll tell my father, then plan revenge", //S
-      //   "Let them be, I have better things to do than waste my time on those people", //R
-      //   "Runaway and cry, but never tell anyone about them", //H
-      // ],
+      choices: [
+        {
+          value: houses.gryffindor.name,
+          description: houses.gryffindor.traits[0],
+        },
+        {
+          value: houses.slytherin.name,
+          description: houses.slytherin.traits[0],
+        },
+        {
+          value: houses.ravenclaw.name,
+          description: houses.ravenclaw.traits[0],
+        },
+        {
+          value: houses.hufflepuff.name,
+          description: houses.hufflepuff.traits[0],
+        },
+      ],
       image: "assets/image-1.gif",
     },
     {
       question:
         "A Muggle confronts you and says that they are sure you are a witch or wizard. Do you:",
-      // choices: [
-      //   "Ask what makes them think so", //R
-      //   "Agree, and ask whether they’d like a free sample of a jinx?", //S
-      //   "Agree, and walk away, leaving them to wonder whether you are bluffing?", //G
-      //   "Tell them that you are worried about their mental health, and offer to call a doctor", //H
-      // ],
+      choices: [
+        {
+          value: houses.gryffindor.name,
+          description: houses.gryffindor.traits[1],
+        },
+        {
+          value: houses.slytherin.name,
+          description: houses.slytherin.traits[1],
+        },
+        {
+          value: houses.ravenclaw.name,
+          description: houses.ravenclaw.traits[1],
+        },
+        {
+          value: houses.hufflepuff.name,
+          description: houses.hufflepuff.traits[1],
+        },
+      ],
       image: "assets/image-2.1.gif",
     },
     {
       question: "What fault do you notice in people that bother you the most?",
-      // choices: [
-      //   "stupidity", //S
-      //   "dishonesty", //H
-      //   "cowardice", //G
-      //   "laziness", //R
-      // ],
+      choices: [
+        {
+          value: houses.gryffindor.name,
+          description: houses.gryffindor.traits[2],
+        },
+        {
+          value: houses.slytherin.name,
+          description: houses.slytherin.traits[2],
+        },
+        {
+          value: houses.ravenclaw.name,
+          description: houses.ravenclaw.traits[2],
+        },
+        {
+          value: houses.hufflepuff.name,
+          description: houses.hufflepuff.traits[2],
+        },
+      ],
       image: "assets/image-3.gif",
     },
     {
       question: "What's the best trait do you think you have?",
-      // choices: [
-      //   "intelligence", //R
-      //   "bravery", //G
-      //   "kindness", //H
-      //   "ambition", //S
-      // ],
+      choices: [
+        {
+          value: houses.gryffindor.name,
+          description: houses.gryffindor.traits[3],
+        },
+        {
+          value: houses.slytherin.name,
+          description: houses.slytherin.traits[3],
+        },
+        {
+          value: houses.ravenclaw.name,
+          description: houses.ravenclaw.traits[3],
+        },
+        {
+          value: houses.hufflepuff.name,
+          description: houses.hufflepuff.traits[3],
+        },
+      ],
       image: "assets/image-5.gif",
     },
     {
       question: "What are you most looking forward to learning at Hogwarts?",
-      // choices: [
-      //   "Secrets about the castle", //G
-      //   "Every area of magic I can", //R
-      //   "All about magical creatures, and how to befriend/care for them", //H
-      //   "Hexes and jinxes", //S
-      // ],
+      choices: [
+        {
+          value: houses.gryffindor.name,
+          description: houses.gryffindor.traits[4],
+        },
+        {
+          value: houses.slytherin.name,
+          description: houses.slytherin.traits[4],
+        },
+        {
+          value: houses.ravenclaw.name,
+          description: houses.ravenclaw.traits[4],
+        },
+        {
+          value: houses.hufflepuff.name,
+          description: houses.hufflepuff.traits[4],
+        },
+      ],
       image: "assets/image-6.gif",
     },
     {
       question: "Which nightmare would frighten you most?",
-      // choices: [
-      //   "stupidity", //S
-      //   "dishonesty", //H
-      //   "cowardice", //G
-      //   "laziness", //R
-      // ],
+      choices: [
+        {
+          value: houses.gryffindor.name,
+          description: houses.gryffindor.traits[5],
+        },
+        {
+          value: houses.slytherin.name,
+          description: houses.slytherin.traits[5],
+        },
+        {
+          value: houses.ravenclaw.name,
+          description: houses.ravenclaw.traits[5],
+        },
+        {
+          value: houses.hufflepuff.name,
+          description: houses.hufflepuff.traits[5],
+        },
+      ],
       image: "assets/dementors.gif",
     },
     {
       question:
         "Late at night, walking alone down the street, you hear a peculiar cry that you believe to have a magical source. What would you do?",
-      // choices: [
-      //   "Proceed with caution", //H
-      //   "Draw your wand and try to discover the source of the noise", //G
-      //   "Withdraw into the shadows to await developments", //R
-      //   "Draw your wand and stand your ground", //S
-      // ],
+      choices: [
+        {
+          value: houses.gryffindor.name,
+          description: houses.gryffindor.traits[6],
+        },
+        {
+          value: houses.slytherin.name,
+          description: houses.slytherin.traits[6],
+        },
+        {
+          value: houses.ravenclaw.name,
+          description: houses.ravenclaw.traits[6],
+        },
+        {
+          value: houses.hufflepuff.name,
+          description: houses.hufflepuff.traits[6],
+        },
+      ],
       image: "assets/image-4.gif",
     },
   ];
 
-  // const random = (min, max) =>
-  //   Math.trunc(Math.random() * (max - min + 1) + min);
-
-  const generateCard = () => {
+  const generateCard = (description, imageSource, choices) => {
+    const cardContainer = document.createElement("div");
+    const imgContainer = document.createElement("div");
+    const form = document.createElement("form");
+    const img = document.createElement("img");
     const question = document.createElement("p");
     const choicesContainer = document.createElement("div");
 
+    //class names
+    cardContainer.className = "card-container";
+    imgContainer.className = "img-container";
     question.className = "question";
     choicesContainer.className = "choices-container";
 
-    question.innerHTML = quizItems[count].question;
-    nextBtn.innerHTML = "Next";
-    for (let i = 0; i < houses.length; i++) {
+    question.innerHTML = description;
+    cardContainer.appendChild(question);
+
+    img.src = imageSource;
+    imgContainer.appendChild(img);
+    cardContainer.appendChild(imgContainer);
+
+    choices.forEach((element) => {
       const inputContainer = document.createElement("div");
       const radio = document.createElement("input");
       const choice = document.createElement("p");
 
+      radio.type = "radio";
+      radio.name = "choice";
+      radio.value = element.value;
+      radio.className = "radio";
+      choice.className = "choice";
       inputContainer.className = "input-container";
-      radio.setAttribute("type", "radio");
-      radio.setAttribute("name", "choice");
-      radio.setAttribute("class", "radio");
 
-      choice.setAttribute("class", "choice");
-
-      choice.innerHTML = houses[i].traits[count];
-      gifImage.setAttribute("src", quizItems[count].image);
+      choice.innerHTML = element.description;
       inputContainer.append(radio, choice);
-      choicesContainer.appendChild(inputContainer);
-      imgContainer.appendChild(gifImage);
+      form.appendChild(inputContainer);
+    });
+    choicesContainer.appendChild(form);
+    cardContainer.appendChild(choicesContainer);
+
+    return cardContainer;
+  };
+
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  const generateQuizCard = () => {
+    const quizCards = document.createElement("div");
+    const button = document.createElement("button");
+    button.innerHTML = "next";
+    quizCards.id = "quizCards";
+    quizCards.className = "quizCards-container";
+    quizCards.appendChild(button);
+    return quizCards;
+  };
+
+  const choicesClicked = ({ index, quizItems }) => (event) => {
+    if (event.target.tagName == "INPUT") {
+      quizStat.chosen[index] = (event.target.value);
+      console.log("hllo", event.target.value);
+      console.log(quizStat);
     }
 
-    cardContainer.append(question, imgContainer, choicesContainer);
-  };
-
-  const showCard = () => {
-    nextBtn.className = "";
-
-    if (count < numberOfQuestions) {
-      generateCard();
-      if (cardContainer.innerHTML !== "") {
-        window.addEventListener("click", handleClick);
-      }
-    } else {
-      // sort();
-
-      // setTimeout(showResult, 5000);
-      console.log(getResult());
-    }
-  };
-
-  const next = () => {
-    // console.log(choice);
-    houses[choice].tally += 1;
-    console.log(`House: ${houses[choice].name} ${houses[choice].tally}`);
-    // answers.push(choice);
-    // console.log((houses[choice].tally += 1));
-    count++;
-    cardContainer.innerHTML = "";
-    showCard();
-  };
-
-  const handleClick = (event) => {
-    if (event.target.getAttribute("class") === "radio") {
-      const selectedAnswer = event.target.parentElement.children[1].innerHTML;
-      console.log(selectedAnswer);
-
-      choice = houses.findIndex((house) =>
-        house.traits.includes(selectedAnswer)
-      );
+    if (event.target.tagName == "BUTTON") {
+      startQiuz({ index: index + 1, quizItems: quizItems });
+      console.log(event.target);
+      console.log("hllo", event.target.value);
+      quizStat.chosen[index] = answer;
+      console.log(quizStat.chosen);
     }
   };
 
-  // const randomizeChoices = () => {
-  //   let choices = []
-  //   for(let i = 0; i < houses.length; i++){
-  //     choices.push(houses[i].traits[count])
-  //   }
-  // }
-  const hasTie = () => {
-    console.log(
-      houses.reduce(
-        (acc, current) => (acc < current ? (acc = current) : (acc = acc)),
-        0
-      )
+  const startQiuz = ({ index, quizItems }) => {
+    const quiz = quizItems[index];
+    const quizCards = document.querySelector("#quizCards");
+    const card = generateCard(quiz.question, quiz.image, quiz.choices);
+
+    quizCards.childNodes.forEach((child) => {
+      if (child.className === "card-container") child.remove();
+    });
+
+    quizCards.appendChild(card);
+    quizCards.addEventListener(
+      "click",
+      choicesClicked({ index: index, quizItems: quizItems })
     );
-  };
-
-  const getResult = () => {
-    console.log("tally:");
-    if (hasTie()) {
-      numberOfQuestions += 1;
-      return showCard();
-    } else {
-      houses.forEach((house) => console.log(house.name + ": " + house.tally));
-      // return houses.reduce((acc, current) =>
-      // console.log (acc[current])
-      // prev.tally > current.tally ?`${prev.name}: ${prev.tally}` : `${current.name}: ${current.tally}`
-      // );
-    }
-  };
-
-  const sort = () => {
-    const loadingText = document.createElement("p");
-    nextBtn.className = "hide";
-    gifImage.setAttribute("src", "assets/sorting-hat.gif");
-    imgContainer.appendChild(gifImage);
-    loadingText.innerHTML = "sorting...";
-    cardContainer.append(imgContainer, loadingText);
-  };
-
-  const showResult = () => {
-    cardContainer.innerHTML = "";
-    console.log("show result");
   };
 
   const openNav = () => {
@@ -277,51 +319,17 @@ window.onload = function onload() {
   };
 
   const proceedToQuiz = () => {
-    // cardContainer.className = "";
-    quizStartContainer.className = "hide";
-    showCard();
-    // generateCard();
+    console.log("proceeding to quiz");
+    const quizStartContainer = document.querySelector("#quiz-start-container");
+    quizStartContainer.style.display = "none";
+
+    const contentContainer = document.querySelector("#content-container");
+    contentContainer.appendChild(generateQuizCard());
+
+    startQiuz({ index: 0, quizItems: quizItems });
   };
 
   hamburgerIcon.addEventListener("click", openNav);
   closeIcon.addEventListener("click", closeNav);
   startButton.addEventListener("click", proceedToQuiz);
-  nextBtn.addEventListener("click", next);
 };
-
-// add card element generator to automatically generarate cards.
-/* 
-#use:
-document.createElement
-
-
-#sample output:
-
-<div class="card-container">
-          <div class="title"><h2>TITLE</h2></div>
-          <div class="img-container">
-            <img src="assets/burger.png" alt="" />
-            <p>
-              Description inserted here <br />
-              caption
-            </p>
-          </div>
-          <div class="buttons">
-            <form action="">
-              <div class="input">
-                <input type="radio" name="choice" value="hi" />hi <br />
-              </div>
-              <div class="input">
-                <input type="radio" name="choice" value="hi" />hi <br />
-              </div>
-              <div class="input">
-                <input type="radio" name="choice" value="hi" />hi <br />
-              </div>
-              <div class="input">
-                <input type="radio" name="choice" value="hi" />hi <br />
-              </div>
-            </form>
-          </div>
-        </div>
-
-*/
