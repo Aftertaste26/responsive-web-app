d30d2d85df84581a7b61cc0eb377b6895db357de
edelynnm <edelynnm@gmail.com>
2020-10-27 08:16:41 +0800

    feat(add get result): add get result

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// const R = require("ramda");
+
 window.onload = function onload() {
   const hamburgerIcon = document.querySelector(".hamburger-menu");
   const closeIcon = document.querySelector(".closebtn");
@@ -10,58 +12,73 @@ window.onload = function onload() {
   const gifImage = document.createElement("img");
   imgContainer.className = "img-container";
   let choice;
-  let answers = [];
-  
+  let numberOfQuestions = 5;
+  let count = 0;
+
+  // let answers = [];
+
   const houses = [
     {
-      house: "Gryffindor",
+      name: "Gryffindor",
       traits: [
         "Stand up to them",
         "Agree, and walk away, leaving them to wonder whether you are bluffing",
-        "cowardice",
-        "Draw your wand and try to discover the source of the noise",
-        "bravery",
+        "Cowardice",
+        "Bravery",
         "Secrets about the castle",
+        "An eye at the keyhole",
+        "Draw your wand and try to discover the source of the noise",
       ],
+      tally: 0,
+      logo: "assets/",
     },
     {
-      house: "Ravenclaw",
+      name: "Ravenclaw",
       traits: [
         "Let them be, I have better things to do than waste my time on those people",
         "Ask what makes them think so",
-        "laziness",
-        "Withdraw into the shadows to await developments",
+        "Laziness",
         "intelligence",
         "Every area of magic I can",
+        "Standing on top of something very high",
+        "Withdraw into the shadows to await developments",
       ],
+      tally: 0,
+      logo: "assets/",
     },
     {
-      house: "Hufflepuff",
+      name: "Hufflepuff",
       traits: [
         "Runaway and cry, but never tell anyone about them",
         "Tell them that you are worried about their mental health, and offer to call a doctor",
-        "dishonesty",
-        "Proceed with caution",
-        "kindness",
+        "Dishonesty",
+        "Kindness",
         "All about magical creatures, and how to befriend/care for them",
+        "Your friends nor your family have any idea who you are",
+        "Proceed with caution",
       ],
+      tally: 0,
+      logo: "assets/",
     },
     {
-      house: "Slytherin",
+      name: "Slytherin",
       traits: [
         "Leave them to what they're doing, I'll tell my father, then plan revenge",
         "Agree, and ask whether they’d like a free sample of a jinx",
-        "stupidity",
-        "Draw your wand and stand your ground",
-        "ambition",
+        "Stupidity",
+        "Ambition",
         "Hexes and jinxes",
+        "Being forced to speak in such a silly voice",
+        "Draw your wand and stand your ground",
       ],
+      tally: 0,
+      logo: "assets/",
     },
   ];
 
   const quizItems = [
     {
-      question: "How would you react if someone picks on you and your friend??",
+      question: "How would you react if someone picks on you and your friend?",
       // choices: [
       //   "Stand up to them", //G
       //   "Leave them to what they're doing, I'll tell my father, then plan revenge", //S
@@ -92,17 +109,6 @@ window.onload = function onload() {
       image: "assets/image-3.gif",
     },
     {
-      question:
-        "Late at night, walking alone down the street, you hear a peculiar cry that you believe to have a magical source. What would you do?",
-      // choices: [
-      //   "Proceed with caution", //H
-      //   "Draw your wand and try to discover the source of the noise", //G
-      //   "Withdraw into the shadows to await developments", //R
-      //   "Draw your wand and stand your ground", //S
-      // ],
-      image: "assets/image-4.gif",
-    },
-    {
       question: "What's the best trait do you think you have?",
       // choices: [
       //   "intelligence", //R
@@ -122,10 +128,31 @@ window.onload = function onload() {
       // ],
       image: "assets/image-6.gif",
     },
+    {
+      question: "Which nightmare would frighten you most?",
+      // choices: [
+      //   "stupidity", //S
+      //   "dishonesty", //H
+      //   "cowardice", //G
+      //   "laziness", //R
+      // ],
+      image: "assets/dementors.gif",
+    },
+    {
+      question:
+        "Late at night, walking alone down the street, you hear a peculiar cry that you believe to have a magical source. What would you do?",
+      // choices: [
+      //   "Proceed with caution", //H
+      //   "Draw your wand and try to discover the source of the noise", //G
+      //   "Withdraw into the shadows to await developments", //R
+      //   "Draw your wand and stand your ground", //S
+      // ],
+      image: "assets/image-4.gif",
+    },
   ];
 
-  let count = 0;
-  const random = (min, max) => Math.trunc(Math.random() * (max - min + 1) + min)
+  // const random = (min, max) =>
+  //   Math.trunc(Math.random() * (max - min + 1) + min);
 
   const generateCard = () => {
     const question = document.createElement("p");
@@ -161,21 +188,25 @@ window.onload = function onload() {
   const showCard = () => {
     nextBtn.className = "";
 
-    // cardContainer.innerHTML = "";
-    // console.log(count);
-    if (count < 5) {
+    if (count < numberOfQuestions) {
       generateCard();
       if (cardContainer.innerHTML !== "") {
         window.addEventListener("click", handleClick);
       }
     } else {
-      sort();
-      setTimeout(showResult, 5000);
+      // sort();
+
+      // setTimeout(showResult, 5000);
+      console.log(getResult());
     }
   };
 
   const next = () => {
-    answers.push(choice);
+    // console.log(choice);
+    houses[choice].tally += 1;
+    console.log(`House: ${houses[choice].name} ${houses[choice].tally}`);
+    // answers.push(choice);
+    // console.log((houses[choice].tally += 1));
     count++;
     cardContainer.innerHTML = "";
     showCard();
@@ -185,8 +216,10 @@ window.onload = function onload() {
     if (event.target.getAttribute("class") === "radio") {
       const selectedAnswer = event.target.parentElement.children[1].innerHTML;
       console.log(selectedAnswer);
-     
-      choice = houses.findIndex(house => house.traits.includes(selectedAnswer));
+
+      choice = houses.findIndex((house) =>
+        house.traits.includes(selectedAnswer)
+      );
     }
   };
 
@@ -196,12 +229,29 @@ window.onload = function onload() {
   //     choices.push(houses[i].traits[count])
   //   }
   // }
+  const hasTie = () => {
+    console.log(
+      houses.reduce(
+        (acc, current) => (acc < current ? (acc = current) : (acc = acc)),
+        0
+      )
+    );
+  };
 
   const getResult = () => {
-    
-    answers.reduce((prev, current) => prev === current ?  )
-  }
-  
+    console.log("tally:");
+    if (hasTie()) {
+      numberOfQuestions += 1;
+      return showCard();
+    } else {
+      houses.forEach((house) => console.log(house.name + ": " + house.tally));
+      // return houses.reduce((acc, current) =>
+      // console.log (acc[current])
+      // prev.tally > current.tally ?`${prev.name}: ${prev.tally}` : `${current.name}: ${current.tally}`
+      // );
+    }
+  };
+
   const sort = () => {
     const loadingText = document.createElement("p");
     nextBtn.className = "hide";
@@ -232,8 +282,6 @@ window.onload = function onload() {
     showCard();
     // generateCard();
   };
-
-
 
   hamburgerIcon.addEventListener("click", openNav);
   closeIcon.addEventListener("click", closeNav);
